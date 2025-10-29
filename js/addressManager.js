d847063e2fb33755c938d94d084047cb66ae93ce
andremejitarian <amejitarian@gmail.com>
2025-10-29 07:56:08 -0300

    Update addressManager.js

--- js/addressManager.js
+++ js/addressManager.js
@@ -160,35 +160,35 @@ class AddressManager {
     }
 
     // Validar campos de endereço
-    validateAddressFields($participant) {
-        const requiredFields = [
-            { selector: '.cep-input', name: 'CEP' },
-            { selector: '.logradouro-input', name: 'Logradouro' },
-            { selector: '.numero-input', name: 'Número' },
-            { selector: '.bairro-input', name: 'Bairro' },
-            { selector: '.cidade-input', name: 'Cidade' },
-            { selector: '.estado-select', name: 'Estado' }
-        ];
+validateAddressFields($participant) {
+    const requiredFields = [
+        { selector: '.cep-input', name: 'CEP' },
+        { selector: '.logradouro-input', name: 'Logradouro' },
+        { selector: '.numero-input', name: 'Número' },
+        { selector: '.bairro-input', name: 'Bairro' },
+        { selector: '.cidade-input', name: 'Cidade' },
+        { selector: '.estado-select', name: 'Estado' }
+    ];
 
-        let isValid = true;
-        let firstErrorField = null;
+    let isValid = true;
+    let firstErrorField = null;
 
-        requiredFields.forEach(field => {
-            const $field = $participant.find(field.selector);
-            const value = $field.val();
+    requiredFields.forEach(field => {
+        const $field = $participant.find(field.selector);
 
-                $field.addClass('error');
-                if (!firstErrorField) {
-                    firstErrorField = $field;
-                }
-                isValid = false;
-            } else {
-                $field.removeClass('error');
+        if (!value) {
+            $field.addClass('error');
+            if (!firstErrorField) {
+                firstErrorField = $field;
             }
-        });
+            isValid = false;
+        } else {
+            $field.removeClass('error');
+        }
+    });
 
-        return { isValid, firstErrorField };
-    }
+    return { isValid, firstErrorField };
+}
 
     // Extrair dados do endereço
     extractAddressData($participant) {
